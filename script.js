9126e29205bacc655a79dfb243921fa78401b0d4
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-19 17:15:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function clicked(){
+	let text = "Do you want to proceed?";
+	if(confirm(text) ==true){
+		alert("You clicked OK. Proceeding...");
+	}
+	else{
+		alert("You clicked Cancel. Exiting...")
+	}
+	 
+} 
